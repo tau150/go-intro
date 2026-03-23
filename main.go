7ba9a02e5497f0ebb43c468a419d1b0e771199b1
Santiago Nuñez <tau150@hotmail.com>
2026-03-23 22:55:39 +0100

    example

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"price-calculator/filemanager"
+	"price-calculator/prices"
+)
+
+func main() {
+	taxRates := []float64{0, 0.7, 0.1, 0.15}
+	for _, taxRate := range taxRates {
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		err := priceJob.Process()
+
+		if err != nil {
+			fmt.Println("Could not process job")
+			fmt.Println(err)
+		}
+	}
+}
